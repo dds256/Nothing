0f08e79860338a83d92fd4df6953a4c2553d2359
Dds2 <85358022+dds256@users.noreply.github.com>
2024-05-28 12:27:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://t.me/akd00bx377",
+    "https://github.com/dds256/Nothing",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
